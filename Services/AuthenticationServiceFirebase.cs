9ce2eff7bf26d17265f6af7b18bd0ff8d2b651ae
SUMAN HUMAGAIN <returnzerosh@gmail.com>
2024-07-26 16:13:39 -0400

    Auth service added

---
+++ Services/AuthenticationServiceFirebase.cs
@@ -0,0 +1,24 @@
+﻿using Firebase.Auth.Providers;
+using Firebase.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory_App.Services
+{
+    public class AuthenticationServiceFirebase
+    {
+        private static AuthenticationServiceFirebase _instance;
+        public static AuthenticationServiceFirebase Instance => _instance ??= new AuthenticationServiceFirebase();
+
+        private FirebaseAuthClient _client;
+
+        private AuthenticationServiceFirebase()
+        {
+        }
+
+        
+    }
+}
