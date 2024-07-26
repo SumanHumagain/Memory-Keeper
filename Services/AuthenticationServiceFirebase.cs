00955f36f883025d191e4c4024f2ac07ccbd66af
SUMAN HUMAGAIN <aamri182@mycambrian.ca>
2024-07-26 16:19:46 -0400

    Implementing authentication service

--- Services/AuthenticationServiceFirebase.cs
+++ Services/AuthenticationServiceFirebase.cs
@@ -19,6 +19,68 @@ namespace Memory_App.Services
         {
         }
 
-        
+        private void InitializeFirebase()
+        {
+            var config = new FirebaseAuthConfig
+            {
+                //ApiKey = AppSettings.FirebaseApiKey,
+                //AuthDomain = AppSettings.FirebaseAuthDomain,
+                Providers = new FirebaseAuthProvider[]
+                {
+                    new EmailProvider()
+                }
+            };
+
+            _client = new FirebaseAuthClient(config);
+        }
+
+        public async Task<(bool success, string message)> SignInAsync(string email, string password)
+        {
+            InitializeFirebase();
+
+            string message;
+            var success = false;
+
+            try
+            {
+                var userCredential = await _client.SignInWithEmailAndPasswordAsync(email, password);
+                message = $"Signed in as {userCredential.User.Info.Email}";
+                success = true;
+            }
+            catch (FirebaseAuthHttpException ex)
+            {
+                message = ex.Reason.ToString() == "Unknown" ? "Incorrect email or password." : ex.Reason.ToString();
+            }
+            catch (Exception ex)
+            {
+                message = $"An error occurred: {ex.Message}";
+            }
+
+            return (success, message);
+        }
+
+        public async Task<(bool success, string message)> SignUpAsync(string email, string password)
+        {
+            InitializeFirebase();
+            string message;
+            var success = false;
+
+            try
+            {
+                var userCredential = await _client.CreateUserWithEmailAndPasswordAsync(email, password);
+                message = $"Account created for {userCredential.User.Info.Email}";
+                success = true;
+            }
+            catch (FirebaseAuthHttpException ex)
+            {
+                message = ex.Reason.ToString();
+            }
+            catch (Exception ex)
+            {
+                message = $"An error occurred: {ex.Message}";
+            }
+
+            return (success, message);
+        }
     }
 }
