9b1b8b835088117081d1bf641aa28f1a5517611f
Amritpal Singh <a00301070@mycambrian.ca>
2024-07-27 12:24:41 -0400

    commented prev. logic as login functionality implemented by adding view model

--- Views/LoginPage.xaml.cs
+++ Views/LoginPage.xaml.cs
@@ -8,23 +8,23 @@
             InitializeComponent();
         }
 
-        private async void OnLoginClicked(object sender, EventArgs e)
-        {
-            string username = UsernameEntry.Text;
-            string password = PasswordEntry.Text;
+        //private async void OnLoginClicked(object sender, EventArgs e)
+        //{
+        //    string username = Email.Text;
+        //    string password = Password.Text;
 
-            // For demonstration purposes, we'll use hardcoded credentials.
-            if (username == "user" && password == "password")
-            {
-                //await DisplayAlert("Login Success", "Welcome to the app!", "OK");
-                // Navigate to the main page or another page after successful login
-                await Navigation.PushAsync(new HomePage());
-            }
-            else
-            {
-                await DisplayAlert("Login Failed", "Invalid username or password", "OK");
-            }
-        }
+        //    // For demonstration purposes, we'll use hardcoded credentials.
+        //    if (username == "user" && password == "password")
+        //    {
+        //        //await DisplayAlert("Login Success", "Welcome to the app!", "OK");
+        //        // Navigate to the main page or another page after successful login
+        //        await Navigation.PushAsync(new HomePage());
+        //    }
+        //    else
+        //    {
+        //        await DisplayAlert("Login Failed", "Invalid username or password", "OK");
+        //    }
+        //}
     }
 
 }
