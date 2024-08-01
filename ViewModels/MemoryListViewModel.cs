fa87235db23fa5c65d725b8441e0cf46df1bfab0
Amritpal Singh <a00301070@mycambrian.ca>
2024-08-01 18:34:49 -0400

    This commit initializes the FirebaseClient with the URL of the Firebase Realtime Database, and sets up an ObservableCollection of MemoryDetail objects. The FirebaseClient is used to interact with the Firebase Realtime Database, while the ObservableCollection is a collection type that provides notifications when items are added, removed, or when the whole list is refreshed. This setup is crucial for handling real-time updates and displaying a list of memory details in the application.

---
+++ ViewModels/MemoryListViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Firebase.Database;
+using Memory_App.Models;
+
+namespace Memory_App.ViewModels
+{
+    public class MemoryListViewModel : BindableObject
+    {
+        private readonly FirebaseClient _firebaseClient;
+
+        public ObservableCollection<MemoryDetail> Memories { get; set; }
+
+        public MemoryListViewModel()
+        {
+            _firebaseClient = new FirebaseClient("https://memorykeeper-8bcf4-default-rtdb.firebaseio.com/");
+            Memories = new ObservableCollection<MemoryDetail>();
+            LoadMemories();
+        }
+
+        private async void LoadMemories()
+        {
+            var memories = await _firebaseClient.Child("MemoryDetail").OnceAsync<MemoryDetail>();
+            foreach (var memory in memories)
+            {
+                Memories.Add(memory.Object);
+            }
+            //var memories = d.GetMemories();
+            //foreach (var memory in memories)
+            //{
+            //    Memories.Add(memory.Object);
+            //}
+        }
+    }
+}
