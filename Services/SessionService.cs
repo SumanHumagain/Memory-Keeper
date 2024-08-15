9f811abab11051a1d50bc154f9b7c4eb5bb99efb
Amritpal Singh <A00301070@mycambrian.ca>
2024-08-14 20:15:41 -0400

    new service added for session

---
+++ Services/SessionService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory_App.Services
+{
+    public interface IUserSessionService
+    {
+        string? Email { get; set; }
+    }
+    public class SessionService : IUserSessionService
+    {
+        public string? Email { get; set; }
+    }
+}
