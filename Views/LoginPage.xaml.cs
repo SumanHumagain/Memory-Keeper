b61cc59c1ed2042481b3d530c27ae265b314c282
Amritpal Singh <A00301070@mycambrian.ca>
2024-08-14 20:13:20 -0400

    Login page logic changes after adding mixpanel services for analytics

--- Views/LoginPage.xaml.cs
+++ Views/LoginPage.xaml.cs
@@ -1,30 +1,54 @@
-﻿namespace Memory_App.Views
+﻿using Memory_App.Global;
+using Memory_App.Services;
+using Memory_App.ViewModels;
+using Mixpanel;
+
+namespace Memory_App.Views
 {
     public partial class LoginPage : ContentPage
     {
-
+        private readonly AnalyticsService _analyticsService;
+        //private readonly MixpanelClient _mixpanelClient;
         public LoginPage()
         {
+            //_mixpanelClient = new MixpanelClient("be5b08d35d1dcfa6dd657f9e7e896ba6");
+            _analyticsService = new AnalyticsService("be5b08d35d1dcfa6dd657f9e7e896ba6");
             InitializeComponent();
         }
+        private async void OnLoginClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var viewModel = BindingContext as LoginViewModel;
 
-        //private async void OnLoginClicked(object sender, EventArgs e)
-        //{
-        //    string username = Email.Text;
-        //    string password = Password.Text;
+                bool isSuccess = await viewModel.SignIn();
+                if (isSuccess)
+                {
+                    //await DisplayAlert("Login Success", "Welcome to memory keeper!", "OK");
 
-        //    // For demonstration purposes, we'll use hardcoded credentials.
-        //    if (username == "user" && password == "password")
-        //    {
-        //        //await DisplayAlert("Login Success", "Welcome to the app!", "OK");
-        //        // Navigate to the main page or another page after successful login
-        //        await Navigation.PushAsync(new HomePage());
-        //    }
-        //    else
-        //    {
-        //        await DisplayAlert("Login Failed", "Invalid username or password", "OK");
-        //    }
-        //}
+                    _analyticsService.TrackEvent("User_Info", new { Email = GlobalVariable.Email });
+                    //_analyticsService.TrackEvent("Login", new Dictionary<string, object>
+                    //{
+                    //    { "User_Info", new { Email = GlobalVariable.Email } }
+                    //});
+
+                    await Navigation.PushAsync(new HomePage());
+
+                }
+
+                // Navigate to the main page or another page after successful login
+                //await Navigation.PushAsync(new HomePage());
+                else
+                {
+                    await DisplayAlert("Login Failed", "Invalid username or password", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Login Failed.", " Try Again!", "OK");
+
+            }
+        }
     }
 
 }
