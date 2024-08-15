972fce116369ad8d52c2cba12ac0090bfa77aa21
Suman Humagain <returnzerosh@gmail.com>
2024-08-14 20:49:35 -0400

    Mixpanel analytics added to project as second cloud service

---
+++ Services/AnalyticsService.cs
@@ -0,0 +1,25 @@
+﻿using Mixpanel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory_App.Services
+{
+
+    public class AnalyticsService
+    {
+        private readonly MixpanelClient _mixpanelClient;
+
+        public AnalyticsService(string apiKey)
+        {
+            _mixpanelClient = new MixpanelClient("be5b08d35d1dcfa6dd657f9e7e896ba6");
+        }
+
+        public void TrackEvent(string eventName, object properties)
+        {
+            _mixpanelClient.TrackAsync(eventName, properties);
+        }
+    }
+}
