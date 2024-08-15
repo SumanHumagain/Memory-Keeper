9250832ab40ef97d7e03c6fdc38a148435fd4f52
Suman Humagain <returnzerosh@gmail.com>
2024-08-14 20:51:08 -0400

    Updates on firebase service, for db and storage of memory

--- Services/AuthenticationServiceFirebase.cs
+++ Services/AuthenticationServiceFirebase.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Memory_App.Services.SessionService;
+using System.Runtime.InteropServices;
+using Memory_App.Global;
 
 namespace Memory_App.Services
 {
@@ -15,20 +18,31 @@ namespace Memory_App.Services
 
         private FirebaseAuthClient _client;
 
-        private AuthenticationServiceFirebase()
-        {
-        }
+        private readonly IUserSessionService _userSessionService;
+        private readonly AnalyticsService _analyticsService;
 
-        private void InitializeFirebase()
+        //GlobalVariable s = new GlobalVariable();
+
+        //public AuthenticationServiceFirebase()
+        //{
+        //    InitializeFirebase();
+        //}
+        public AuthenticationServiceFirebase([Optional] IUserSessionService userSessionService)
+        {
+            InitializeFirebase();
+            _analyticsService = new AnalyticsService("be5b08d35d1dcfa6dd657f9e7e896ba6");
+            _userSessionService = userSessionService;
+        }
+        public void InitializeFirebase()
         {
             var config = new FirebaseAuthConfig
             {
-                //ApiKey = AppSettings.FirebaseApiKey,
-                //AuthDomain = AppSettings.FirebaseAuthDomain,
+                ApiKey = "AIzaSyAGEq2UQwNHIR4Aflo0jBma9axkyfJlMX4",
+                AuthDomain = "memorykeeper-8bcf4.firebaseapp.com",
                 Providers = new FirebaseAuthProvider[]
-                {
+             {
                     new EmailProvider()
-                }
+             }
             };
 
             _client = new FirebaseAuthClient(config);
@@ -37,7 +51,7 @@ namespace Memory_App.Services
         public async Task<(bool success, string message)> SignInAsync(string email, string password)
         {
             InitializeFirebase();
-
+      
             string message;
             var success = false;
 
@@ -45,6 +59,8 @@ namespace Memory_App.Services
             {
                 var userCredential = await _client.SignInWithEmailAndPasswordAsync(email, password);
                 message = $"Signed in as {userCredential.User.Info.Email}";
+                // Store user name
+                GlobalVariable.Email = userCredential.User.Info.Email;
                 success = true;
             }
             catch (FirebaseAuthHttpException ex)
@@ -69,6 +85,9 @@ namespace Memory_App.Services
             {
                 var userCredential = await _client.CreateUserWithEmailAndPasswordAsync(email, password);
                 message = $"Account created for {userCredential.User.Info.Email}";
+                GlobalVariable.Email = userCredential.User.Info.Email;
+                _analyticsService.TrackEvent("Register", new { Email = GlobalVariable.Email });
+
                 success = true;
             }
             catch (FirebaseAuthHttpException ex)
@@ -82,5 +101,6 @@ namespace Memory_App.Services
 
             return (success, message);
         }
+
     }
 }
---
+++ Services/FirebaseDb.cs
@@ -0,0 +1,28 @@
+﻿using Firebase.Auth.Providers;
+using Firebase.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Firebase.Database;
+using Memory_App.Models;
+
+namespace Memory_App.Services
+{
+    public class FirebaseDb
+    {
+        private readonly FirebaseClient _firebaseClient;
+
+        public FirebaseDb()
+        {
+            _firebaseClient = new FirebaseClient("https://memorykeeper-8bcf4-default-rtdb.firebaseio.com/");
+        }
+
+        public async Task<MemoryDetail> GetMemories()
+        {
+           var memories = await _firebaseClient.Child("MemoryDetail").OnceAsync<MemoryDetail>();
+           return (MemoryDetail)memories;
+        }
+    }
+}
