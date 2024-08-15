be4d5d19271bf02ce1b55513a743026a12589aa3
Amritpal Singh <A00301070@mycambrian.ca>
2024-08-14 20:15:21 -0400

    Updated model entity and memory detail

---
+++ Models/MemoryDetail.cs
@@ -0,0 +1,16 @@
+﻿//using SQLite;
+
+namespace Memory_App.Models
+{
+
+        public class MemoryDetail : IEntity
+        {
+            //[PrimaryKey, AutoIncrement]
+            public int Id { get; set; }
+            public string Email { get; set; }
+            public string Location { get; set; }
+            public string Caption { get; set; }
+            public string Image { get; set; }
+            public Stream ImageUploaded { get; set; }
+        }
+}
